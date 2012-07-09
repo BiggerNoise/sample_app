afe8c60119fede22b0721bc8fc34101410493497
Andy Davis <andy.davis@koanhealth.com>
2012-07-09 17:19:12 -0500

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '41fd16a696972e2f1bfd31db5a1e28820ff9cbb4caf7c83d386c73f1aefd214786b2e048a9ba8867deb2e3f4c9b2a7c65f94e7677109e9ceaf7fed72dbdf1d8b'
