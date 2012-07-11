12edbdcd9ed6ae68ab712b5392151b98f8c234db
Andy Davis <andy.davis@koanhealth.com>
2012-07-11 15:59:27 -0500

    Finished Chapter 7 Excercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,6 +15,18 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_selector('#error_explanation') }
+        it { should have_selector('#error_explanation div.alert', text: 'The form contains') }
+        (['Name', 'Email', 'Password', 'Password confirmation']).each do |field_name|
+          it { should have_selector('#error_explanation ul li', text: field_name) }
+        end
+      end
+
     end
 
     describe "with valid information" do
@@ -28,6 +40,16 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('h1',    text: user.name) }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
 
